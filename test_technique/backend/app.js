66983fc96612ab80c278edfabcf80f4fa4093f16
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2021-10-29 18:49:31 +0200

    Add files via upload
    API rest login signup

---
+++ test_technique/backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use(bodyParser.json());
+  
+mongoose.connect('mongodb+srv://mehdi:mehdi@cluster0.pmixz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+  app.use('/api/auth', userRoutes);
+
+  
+module.exports = app;
