ca7028c50e686d93f28def4062a2a6fff7cbb901
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2021-10-30 20:30:47 +0200

    Add files via upload
    API REST post

---
+++ test-technique/backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use(bodyParser.json());
+  
+mongoose.connect('mongodb+srv://mehdi:mehdi@cluster0.pmixz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+  app.use('/api/auth', userRoutes);
+  app.use('/api/post', postRoutes);
+
+
+  
+module.exports = app;
